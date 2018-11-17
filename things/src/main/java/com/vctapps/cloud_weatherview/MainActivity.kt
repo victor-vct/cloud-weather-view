25f2603af5c91840edad7865411c6f27e1bc30d7
Victor Vieira Paulino <victorvieira302@gmail.com>
2018-11-17 18:33:03 -0200

    Initial project setup
    Signed-off-by: Victor Vieira Paulino <victorvieira302@gmail.com>

---
+++ things/src/main/java/com/vctapps/cloud_weatherview/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.vctapps.cloud_weatherview
+
+import android.app.Activity
+import android.os.Bundle
+
+class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+}
