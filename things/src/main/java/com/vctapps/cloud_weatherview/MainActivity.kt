61a99a0f7a75b4b834eec5fa3e5a439f66ea6bcf
Victor Vieira Paulino <victorvieira302@gmail.com>
2018-11-20 22:15:10 -0200

    refactor activity (temp) to show data in screen and in led
    Signed-off-by: Victor Vieira Paulino <victorvieira302@gmail.com>

--- things/src/main/java/com/vctapps/cloud_weatherview/MainActivity.kt
+++ things/src/main/java/com/vctapps/cloud_weatherview/MainActivity.kt
@@ -2,11 +2,47 @@ package com.vctapps.cloud_weatherview
 
 import android.app.Activity
 import android.os.Bundle
+import android.util.Log
+import com.vctapps.cloud_weatherview.data.forecast.openWeather.OpenWeatherDataSource
+import com.vctapps.cloud_weatherview.di.networkModule
+import com.vctapps.cloud_weatherview.domain.WeatherState
+import com.vctapps.cloud_weatherview.presentation.hardware.WeatherLedView
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+import kotlinx.android.synthetic.main.weather_forecast.*
+import org.koin.android.ext.android.inject
+import org.koin.android.ext.android.startKoin
 
 class MainActivity : Activity() {
 
+    private val openWeatherDataSource: OpenWeatherDataSource by inject()
+
+    private val weatherLeds = WeatherLedView()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.weather_forecast)
+
+        startKoin(this, listOf(networkModule))
+
+        weatherLeds.onStart()
+
+        openWeatherDataSource
+                .forecast()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe({ response ->
+                    Log.d("State", response.toString())
+                    textWeatherForecast.text = "${response.temperature}ºC"
+                    weatherLeds.onChangeState(response.weather)
+                }, { error ->
+                    Log.e("State", error.message)
+                    textWeatherForecast.text = "ERROR :("
+                })
     }
 
+    override fun onDestroy() {
+        weatherLeds.onStop()
+        super.onDestroy()
+    }
 }
