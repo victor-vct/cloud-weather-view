319e87821c80e81384344a6202632d2207c35a65
Victor Vieira <victorvieira302@gmail.com>
2019-01-04 00:03:27 -0200

    add sample app to control cloud state
    Signed-off-by: Victor Vieira <victorvieira302@gmail.com>

--- mobile/src/main/java/com/vctapps/cloud_weatherview/MainActivity.kt
+++ mobile/src/main/java/com/vctapps/cloud_weatherview/MainActivity.kt
@@ -2,11 +2,23 @@ package com.vctapps.cloud_weatherview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.FirebaseApp
+import com.google.firebase.database.FirebaseDatabase
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val database = FirebaseDatabase.getInstance().getReference("cloud").child("state")
+
+        buttonClear.setOnClickListener { database.setValue(0) }
+        buttonFewClouds.setOnClickListener { database.setValue(1) }
+        buttonClouds.setOnClickListener { database.setValue(2) }
+        buttonRain.setOnClickListener { database.setValue(3) }
+        buttonThunderstorm.setOnClickListener { database.setValue(4) }
+
     }
 }
